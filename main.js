64aff38c3e19ed9a0158fa0572d1a6543d3e36d1
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-06-10 18:38:38 +0200

    milestone 1 - less flawed
    pie chart complete
    bar chart x axis markers are not ordered

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-
+// var monthlyIncome = {};
   $.ajax({
     'url': 'http://157.230.17.132:4031/sales',
     'method': 'get',
     'success': function(data) {
-      salesPerMonth(data);
-      console.log(monthlyIncome);
-      // chart('monthly', 'line', monthlyIncome);
-      totalSalesPerEmployee(data);
-      // chart('sellers', 'pie', salespeopleTotals);
+      let salesTotal = totalSales(data);
+      console.log(salesTotal);
+      let monthlyIncome = salesPerMonth(data);
+      chart('monthly', 'line', monthlyIncome);
+      let salespeopleTotals = totalSalesPerEmployee(data, salesTotal);
+      chart('sellers', 'pie', salespeopleTotals);
     },
     'error': function() {
       alert('Apparently a rabbit ate your number...');
@@ -18,11 +19,11 @@ $(document).ready(function() {
 function chart(chartID, chartType, processedData) {
   var ctx = $(`#${chartID}`)[0].getContext('2d');
   var chartID = new Chart(ctx, {
-    type: 'bar',
+    type: chartType,
     data: {
       labels: Object.keys(processedData),
       datasets: [{
-        label: '# of Votes',
+        label: 'Volume of Sales per Month',
         data: Object.values(processedData),
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
@@ -56,32 +57,49 @@ function chart(chartID, chartType, processedData) {
     }]
   },
   options: {
+      legend: {
+        // display: false,
+        position: 'bottom',
+      },
       scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
     }
+  }
   });
 }
 
-function salesPerMonth(data) {
-  let monthlyIncome = {};
+function salesPerMonth(data, obj) {
+  let monthlyIncome = {
+    '01': 0,
+    '02': 0,
+    '03': 0,
+    '04': 0,
+    '05': 0,
+    '06': 0,
+    '07': 0,
+    '08': 0,
+    '09': 0,
+    '10': 0,
+    '11': 0,
+    '12': 0,
+  };
   for (var i = 0; i < data.length; i++) {
     let currentMonth = data[i].date.substr(3,2);
-    if (!monthlyIncome.hasOwnProperty(currentMonth)) {
-      monthlyIncome[currentMonth] = data[i].amount;
-    } else {
-      monthlyIncome[currentMonth] += data[i].amount;
-    }
+    console.log(currentMonth);
+    monthlyIncome[currentMonth] += data[i].amount;
   }
   console.log(monthlyIncome);
   return monthlyIncome;
 }
 
-function totalSalesPerEmployee(data) {
+function totalSales(data) {
+  let salesTotal = 0;
+  for (var i = 0; i < data.length; i++) {
+    salesTotal += data[i].amount;
+  }
+  return salesTotal;
+}
+
+function totalSalesPerEmployee(data, totalSales) {
   let salespeopleTotals = {};
   for (var i = 0; i < data.length; i++) {
     let currentSalesman = data[i].salesman;
@@ -90,6 +108,15 @@ function totalSalesPerEmployee(data) {
     } else {
       salespeopleTotals[currentSalesman] += data[i].amount;
     }
+  }
+  for (var key in salespeopleTotals) {
+    let totalNr = salespeopleTotals[key];
+    console.log(totalNr);
+    let percentage = (totalNr / totalSales) * 100;
+    let chartPercent = percentage.toFixed(2);
+    console.log(chartPercent);
+    salespeopleTotals[key] = chartPercent;
+    console.log(salespeopleTotals);
   }
   return salespeopleTotals;
 }
